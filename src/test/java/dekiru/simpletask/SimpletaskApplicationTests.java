90c385bbddcd169b2b3a064ad0c971f50473ff04
Xiaodan Mao <xmao@zuora.com>
2023-01-22 19:41:43 +0800

    init

---
+++ src/test/java/dekiru/simpletask/SimpletaskApplicationTests.java
@@ -0,0 +1,11 @@
+package dekiru.simpletask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpletaskApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+}
