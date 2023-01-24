6311d8d931ade1442c9eaec574c86395335840ef
Frederick-S <mao_xiaodan@hotmail.com>
2023-01-24 10:44:34 +0800

    init

--- src/main/java/dekiru/simpletask/Application.java
+++ src/main/java/dekiru/simpletask/Application.java
@@ -3,9 +3,12 @@ package dekiru.simpletask;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main entry of the app.
+ */
 @SpringBootApplication
-public class SimpletaskApplication {
-	public static void main(String[] args) {
-		SpringApplication.run(SimpletaskApplication.class, args);
-	}
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
---
+++ src/main/java/dekiru/simpletask/controller/BaseController.java
@@ -0,0 +1,11 @@
+package dekiru.simpletask.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * The base controller.
+ */
+public class BaseController {
+    protected Logger logger = LoggerFactory.getLogger(getClass());
+}
