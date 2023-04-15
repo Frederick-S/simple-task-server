b1ad2b76596421e57b73a91f23854239d50ba047
Frederick-S <mao_xiaodan@hotmail.com>
2023-04-15 17:55:10 +0800

    ut: setup test

--- src/main/java/dekiru/simpletask/Application.java
+++ src/main/java/dekiru/simpletask/Application.java
@@ -2,11 +2,15 @@ package dekiru.simpletask;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisIndexedHttpSession;
 
 /**
  * The main entry of the app.
  */
 @SpringBootApplication
+@EnableRedisIndexedHttpSession
+@EnableScheduling
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- src/test/java/dekiru/simpletask/BaseTest.java
+++ src/test/java/dekiru/simpletask/BaseTest.java
@@ -1,9 +1,11 @@
 package dekiru.simpletask;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureMockMvc
 public abstract class BaseTest {
 }
