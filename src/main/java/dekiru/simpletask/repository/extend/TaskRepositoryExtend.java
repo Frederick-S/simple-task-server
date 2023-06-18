0ccd05a3548c643fb23331221cc018f7323ea6e5
Frederick-S <mao_xiaodan@hotmail.com>
2023-06-18 10:45:54 +0800

    fix: lint

--- src/main/java/dekiru/simpletask/repository/extend/TaskRepositoryExtend.java
+++ src/main/java/dekiru/simpletask/repository/extend/TaskRepositoryExtend.java
@@ -10,11 +10,21 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+/**
+ * The extended repository that manipulates task entities.
+ */
 @Repository
 public class TaskRepositoryExtend {
     @Autowired
     private TaskRepository taskRepository;
 
+    /**
+     * Find tasks.
+     *
+     * @param page     The page number
+     * @param pageSize Count of ting practices returned in each page
+     * @return List of {@link Task}
+     */
     public List<Task> findAll(int page, int pageSize) {
         Sort sort = Sort.by(Sort.Direction.DESC, "createdAt");
         Pageable pageable = PageRequest.of(page - 1, pageSize, sort);
